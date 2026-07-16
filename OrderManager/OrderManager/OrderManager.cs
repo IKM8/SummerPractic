a30a48b62b0f995db3e109700fbbf1b9d940eed1
Nikita <glushkow.nikita7v@gmail.com>
2026-07-16 15:47:37 +0300

    OrderManager: оформление заказов с подтверждением

---
+++ OrderManager/OrderManager/OrderManager.cs
@@ -0,0 +1,34 @@
+﻿
+void RequestData()
+{
+    Console.WriteLine( "Внимание, производится оформление заказа" );
+    Console.WriteLine( "Введите название товара" );
+    string? product = Console.ReadLine();
+    Console.WriteLine( "Введите количество товара" );
+    string? amountStr = Console.ReadLine();
+    int amount = int.TryParse( amountStr, out int parsedAmount ) ? parsedAmount : 0;
+    Console.WriteLine( "Введите ваше имя" );
+    string? name = Console.ReadLine();
+    Console.WriteLine( "Введите адрес доставки" );
+    string? address = Console.ReadLine();
+    string result = ConfirmData( product ?? "", amount, name ?? "", address ?? "" );
+    Console.WriteLine( result );
+}
+string ConfirmData( string product, int amount, string name, string address )
+{
+    Console.WriteLine( $"Здравствуйте, {name}, вы заказали {amount} {product} на адрес {address}, все верно?" );
+    string? answer = Console.ReadLine();
+    switch ( ( answer ?? "" ).ToLower() )
+    {
+        case "yes":
+            DateTime deliveryDate = DateTime.Now.AddDays( 3 );
+            string result = $"{name}! Ваш заказ {product} в количестве {amount} оформлен! Ожидайте доставку по адресу {address} к {deliveryDate:dd.MM.yyyy}";
+            return result;
+        case "no":
+            return "Оформление заказа отменено";
+        default:
+            return "Неправильный ввод. Оформление заказа отменено";
+    }
+}
+
+RequestData();
