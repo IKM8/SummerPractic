3b16027ca3e2b69c386f1473658b6cac1aed78b2
Nikita <glushkow.nikita7v@gmail.com>
2026-07-16 15:47:21 +0300

    SImpleCalculator: калькулятор с базовыми операциями

---
+++ SImpleCalculator/SImpleCalculator/SimpleCalculator.cs
@@ -0,0 +1,52 @@
+﻿
+Console.WriteLine( "Введите математическое выражение" );
+string? input = Console.ReadLine();
+if ( string.IsNullOrEmpty( input ) )
+{
+    Console.WriteLine( "Ввод пуст" );
+    return;
+}
+int pos = -1;
+if ( input.Contains( '+' ) )
+    pos = input.IndexOf( '+' );
+else if ( input.Contains( '-' ) )
+    pos = input.IndexOf( '-' );
+else if ( input.Contains( '/' ) )
+    pos = input.IndexOf( '/' );
+else if ( input.Contains( '*' ) )
+    pos = input.IndexOf( '*' );
+if ( pos == -1 )
+{
+    Console.WriteLine( "Не найдена операция" );
+    return;
+}
+int firstOperand = int.Parse( input.Substring( 0, pos ) );
+int secondOperand = int.Parse( input.Substring( pos + 1 ) );
+string operation = input[ pos ].ToString();
+
+
+float calculate( float firstOperand, float secondOperand, string operation )
+{
+    switch ( operation )
+    {
+        case "+":
+            return firstOperand + secondOperand;
+        case "-":
+            return firstOperand - secondOperand;
+        case "*":
+            return firstOperand * secondOperand;
+        case "/":
+            if ( secondOperand == 0 )
+            {
+                Console.WriteLine( "Ошибка деления на ноль" );
+                return 0;
+            }
+            return ( float )firstOperand / secondOperand;
+        default:
+            Console.WriteLine( "Неправильная операция" );
+            return 0;
+    }
+}
+
+float result = calculate( firstOperand, secondOperand, operation );
+Console.WriteLine( $"Результат {result}" );
