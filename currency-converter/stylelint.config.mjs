5c7fa6fbe57196810bcdb3e4683e16375edc2582
Nikita <glushkow.nikita7v@gmail.com>
2026-08-17 10:42:00 +0300

    Настроил проект currency-converter: Vite, React, TypeScript и линтеры

---
+++ currency-converter/stylelint.config.mjs
@@ -0,0 +1,15 @@
+export default {
+  extends: [
+    'stylelint-config-standard-scss',
+    'stylelint-config-css-modules',
+  ],
+  plugins: [
+    'stylelint-order',
+    'stylelint-scss',
+    'stylelint-use-logical',
+  ],
+  rules: {
+    'order/properties-alphabetical-order': true,
+    'csstools/use-logical': ['always', { except: ['width', 'height'] }],
+  },
+}
