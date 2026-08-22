0dad8467e75a44677dcdef1475bb8202e298e130
Nikita <glushkow.nikita7v@gmail.com>
2026-08-23 01:19:58 +0300

    feat: график с периодами и список валютных пар

---
+++ currency-converter/src/components/CurrencyChart/CurrencyChart.tsx
@@ -0,0 +1,28 @@
+import chartImage from '../../assets/hero.png';
+import { Button } from '../Button/Button';
+import styles from './CurrencyChart.module.scss';
+
+type CurrencyChartProps = {
+  periods: number[];
+  currentPeriod: number;
+};
+
+export const CurrencyChart = ({ periods, currentPeriod }: CurrencyChartProps) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.periods}>
+        {periods.map((period) => (
+          <Button
+            key={period}
+            variant={period === currentPeriod ? 'active' : 'default'}
+            className={styles['period-button']}
+          >
+            {period} MIN
+          </Button>
+        ))}
+      </div>
+
+      <img className={styles.chart} src={chartImage} alt="Exchange rate chart" />
+    </div>
+  );
+};
