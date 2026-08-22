2198ff3689da9a62ed7325e47cc68500e5228639
Nikita <glushkow.nikita7v@gmail.com>
2026-08-23 02:17:12 +0300

    chore: удалил старый stylelint-конфиг, оставил один из задания

--- currency-converter/stylelint.config.mjs
+++
@@ -1,8 +0,0 @@
-export default {
-  extends: ['stylelint-config-standard-scss', 'stylelint-config-css-modules'],
-  plugins: ['stylelint-order', 'stylelint-scss', 'stylelint-use-logical'],
-  rules: {
-    'order/properties-alphabetical-order': true,
-    'csstools/use-logical': ['always', { except: ['width', 'height'] }]
-  }
-};
